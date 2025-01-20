20b10ffb75b7307228ce4252d4bbd9f0ed05618b
Victor Arseniu <victor.arseniu@gmail.com>
2025-01-20 20:04:58 +0200

    Initial commit

---
+++ apps/mineclone/main.cpp
@@ -0,0 +1,8 @@
+#include "mineclone.h"
+
+int main()
+{
+  say("Hello, world!\n");
+  return 0;
+}
+
---
+++ src/mineclone.cpp
@@ -0,0 +1,9 @@
+#include "mineclone.h"
+
+#include <stdio.h>
+
+void say(const char *msg)
+{
+  printf(msg);
+}
+
