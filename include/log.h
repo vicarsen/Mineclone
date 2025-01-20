81021a7e3827bd5543bd70482ab6d7784cbc2994
Victor Arseniu <victor.arseniu@gmail.com>
2025-01-20 21:46:43 +0200

    Added conditional logging & dummy def for debug

--- include/log.h
+++ include/log.h
@@ -17,6 +17,8 @@
 #define DECLARE_LOG_CATEGORY(category) namespace mc { extern std::shared_ptr<spdlog::logger> __logger__##category; };
 #define DEFINE_LOG_CATEGORY(category) namespace mc { std::shared_ptr<spdlog::logger> __logger__##category = logger_create(#category); };
 
+#ifndef NDEBUG
+
 #define LOG_TRACE(category, ...) SPDLOG_LOGGER_TRACE(mc::__logger__##category, __VA_ARGS__);
 #define LOG_DEBUG(category, ...) SPDLOG_LOGGER_DEBUG(mc::__logger__##category, __VA_ARGS__);
 #define LOG_INFO(category, ...) SPDLOG_LOGGER_INFO(mc::__logger__##category, __VA_ARGS__);
@@ -24,6 +26,32 @@
 #define LOG_ERROR(category, ...) SPDLOG_LOGGER_ERROR(mc::__logger__##category, __VA_ARGS__);
 #define LOG_CRITICAL(category, ...) SPDLOG_LOGGER_CRITICAL(mc::__logger__##category, __VA_ARGS__);
 #define LOG_ASSERT(condition, category, ...) if (!(condition)) { SPDLOG_LOGGER_CRITICAL(mc::__logger__##category, __VA_ARGS__); assert(0); }
+
+#define LOG_TRACE_IF(condition, category, ...) if (condition) { SPDLOG_LOGGER_TRACE(mc::__logger__##category, __VA_ARGS__); }
+#define LOG_DEBUG_IF(condition, category, ...) if (condition) { SPDLOG_LOGGER_DEBUG(mc::__logger__##category, __VA_ARGS__); }
+#define LOG_INFO_IF(condition, category, ...) if (condition) { SPDLOG_LOGGER_INFO(mc::__logger__##category, __VA_ARGS__); }
+#define LOG_WARN_IF(condition, category, ...) if (condition) { SPDLOG_LOGGER_WARN(mc::__logger__##category, __VA_ARGS__); }
+#define LOG_ERROR_IF(condition, category, ...) if (condition) { SPDLOG_LOGGER_ERROR(mc::__logger__##category, __VA_ARGS__); }
+#define LOG_CRITICAL_IF(condition, category, ...) if (condition) { SPDLOG_LOGGER_CRITICAL(mc::__logger__##category, __VA_ARGS__); }
+
+#else
+
+#define LOG_TRACE(category, ...)
+#define LOG_DEBUG(category, ...)
+#define LOG_INFO(category, ...)
+#define LOG_WARN(category, ...)
+#define LOG_ERROR(category, ...)
+#define LOG_CRITICAL(category, ...)
+#define LOG_ASSERT(condition, category, ...)
+
+#define LOG_TRACE_IF(condition, category, ...)
+#define LOG_DEBUG_IF(condition, category, ...)
+#define LOG_INFO_IF(condition, category, ...)
+#define LOG_WARN_IF(condition, category, ...)
+#define LOG_ERROR_IF(condition, category, ...)
+#define LOG_CRITICAL_IF(condition, category, ...)
+
+#endif
 
 DECLARE_LOG_CATEGORY(Default);
 
