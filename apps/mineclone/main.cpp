7c82c0d3be2387a06353ed2099c5928ce6adeda8
Victor-Stefan Arseniu <victor.arseniu@gmail.com>
2025-02-28 17:18:47 +0200

    Added version struct

--- apps/mineclone/main.cpp
+++ apps/mineclone/main.cpp
@@ -1,10 +1,17 @@
 #include <mineclonelib/cvar.h>
+#include <mineclonelib/misc.h>
 
 static mc::cvar<const char *> app_name("Mineclone", "app/name",
 				       "The name of the application");
 
+static mc::cvar<mc::version> app_version({ 0, 1, 0 }, "app/version",
+					 "The version of the application");
+
 static mc::cvar<const char *> engine_name("Mineclone Engine", "engine/name",
 					  "The name of the engine");
+
+static mc::cvar<mc::version> engine_version({ 0, 1, 0 }, "engine/version",
+					    "The version of the engine");
 
 int main()
 {
---
+++ include/mineclonelib/misc.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <cstdint>
+
+namespace mc
+{
+struct version {
+	uint32_t major, minor, patch;
+};
+}
